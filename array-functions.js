100503b09433060ad4566a5cd7989d1e2292f351
sinan <aslansinan586@gmail.com>
2022-12-24 23:53:51 +0300

    array functionsns

---
+++ array-functions.js
@@ -0,0 +1,53 @@
+const users = [
+        {
+        name:"sinan",
+        age:20
+        },
+        {
+        name:"tuğba",
+        age:23
+        },
+];
+/*
+push
+map
+find
+filter
+some 
+every
+includes
+*/
+//push
+// users.push('Ayşe');
+// users.push('ali');
+// console.log(users);
+
+//map
+// for (let i = 0; i < users.length; i++) {
+//     console.log(users[i]);
+// }
+//yıkardaki ile aynı işi yapar
+// users.map((item) => {
+//     console.log(item);
+// })
+
+//Find
+// const findIt = users.find((item) => item.name  === 'sinan' && item.age >=10);
+// console.log(findIt);
+
+//filter
+// const filtered = users.filter((item) => item.name === 'sinan' && item.age >10);
+// console.log(filtered);
+
+//sum
+// const some = users.some((item) => item.age === 10);
+// console.log(some);
+
+//EVERY
+// const every = users.every((item) => item.age>10);
+// console.log(every   )
+
+//includes
+// const meyveler = ['elma','üzüm','çilek'];
+// const isIncluded = meyveler.includes('elma');
+// console.log(isIncluded);
